bc35269c2d0284333f9be2ff2a6dbda3748a2661
realcathode <ion.miron@tutanota.com>
2024-11-10 15:41:52 +0200

    Add example usage of hex_to_base64 to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
+use cryp;
 
+fn main() {
+    let s = String::from("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d");
+
+    if let Ok(a) = cryp::hex_to_base64(&s) {
+        println!("HEX: {}\nBASE64: {}", s, a);
+    }
 }
