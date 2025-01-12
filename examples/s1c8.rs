c5146748e675da5f10a51c28d280976896123b2d
realcathode <ion.miron@tutanota.com>
2025-01-12 22:11:42 +0200

    Add `examples/s1c8.rs` demonstrating solution for 8th challenge

---
+++ examples/s1c8.rs
@@ -0,0 +1,48 @@
+use std::collections::HashSet;
+use std::io::{self, BufReader, BufRead};
+use std::fs::File;
+use colored::*;
+
+fn main() -> io::Result<()> {
+    let filename = "./8.txt";
+    let file = File::open(filename)?;
+    let reader = BufReader::new(file);
+    let mut count = 0;
+
+    for line in reader.lines() {
+        count = count + 1;
+        let line = line?;
+
+        let mut seen = HashSet::new();
+        let mut duplicates: Vec<&str> = vec![];
+
+        let chunks: Vec<&str> = line.as_bytes()
+            .chunks(32)
+            .map(|chunk| std::str::from_utf8(chunk).unwrap())
+            .collect();
+
+        for chunk in &chunks {
+            if !seen.insert(*chunk) {
+                duplicates.push(*chunk);
+            }
+        }
+
+        let unique_duplicates: HashSet<_> = duplicates.into_iter().collect();
+
+        if !unique_duplicates.is_empty() {
+            print!("Line: {}\n", count);
+            
+            for chunk in chunks {
+                for duplicate in &unique_duplicates {
+                    if chunk.contains(*duplicate) {
+                        print!("{}", chunk.red());
+                    }else {
+                        print!("{}", chunk);
+                    }
+                }
+            }
+            println!();
+        }
+    }
+    Ok(())
+}
