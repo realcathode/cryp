5c037906c7dca4b47886bfd46c8bae457bfb74c2
realcathode <ion.miron@tutanota.com>
2025-01-13 16:18:34 +0200

    Delete `examples` folder

--- examples/s1c5.rs
+++
@@ -1,8 +0,0 @@
-use cryp::{bytes_to_hexstr, xor_repeating};
-
-fn main() {
-    let s = "Burning 'em, if you ain't quick and nimble
-I go crazy when I hear a cymbal";
-    let result = xor_repeating(s.as_bytes(), "ICE".as_bytes());
-    println!("{}", bytes_to_hexstr(&result));
-}
